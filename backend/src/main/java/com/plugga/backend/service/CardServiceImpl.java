428a8df0303df776181e758a3ac5db55955b3a98
Connor Barrett <connor.barrett.dev@gmail.com>
2019-08-29 18:04:56 -0400

    completed CardService and CardServiceImpl

--- backend/src/main/java/com/plugga/backend/service/CardServiceImpl.java
+++ backend/src/main/java/com/plugga/backend/service/CardServiceImpl.java
@@ -3,34 +3,41 @@ package com.plugga.backend.service;
 import com.plugga.backend.dao.CardDAO;
 import com.plugga.backend.entity.Card;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class CardServiceImpl implements CardService {
-    private CardDAO carDAO;
+    private CardDAO cardDAO;
 
     @Autowired
     public CardServiceImpl(final CardDAO carDAO) {
-        this.carDAO = carDAO;
+        this.cardDAO = carDAO;
     }
 
     @Override
+    @Transactional
     public List<Card> findAll() {
-        return carDAO.findAll();
+        return cardDAO.findAll();
     }
 
     @Override
+    @Transactional
     public Card findById(final int id) {
-        return null;
+        return cardDAO.findById(id);
     }
 
     @Override
+    @Transactional
     public void save(final Card card) {
-
+        cardDAO.save(card);
     }
 
     @Override
+    @Transactional
     public void deleteById(final int id) {
-
+        cardDAO.deleteById(id);
     }
 }
