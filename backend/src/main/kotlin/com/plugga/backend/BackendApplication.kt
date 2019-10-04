bc44a9f904618de18d6b5e3bd9e926e5f81d08f7
Connor Barrett <connor.barrett.dev@gmail.com>
2019-10-04 08:23:26 -0400

    completed migration to kotlin

--- backend/src/main/kotlin/com/plugga/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/plugga/backend/BackendApplication.kt
@@ -1,15 +1,11 @@
 package com.plugga.backend
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BackendApplication {
+class BackendApplication
 
-//    @JvmStatic
-    fun main(args: Array<String>) {
-//        SpringApplication.run(BackendApplication::class.java, *args)
-        runApplication<BackendApplication>(*args)
-    }
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
 }
