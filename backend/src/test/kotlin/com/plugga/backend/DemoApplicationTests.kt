2e4706e4155d0666a4955c62c49ba1d9c363a7dd
Connor Barrett <connor.barrett.dev@gmail.com>
2019-11-29 10:09:13 -0500

    basic UserController.getById test

--- backend/src/test/kotlin/com/plugga/backend/DemoApplicationTests.kt
+++ backend/src/test/kotlin/com/plugga/backend/DemoApplicationTests.kt
@@ -1,5 +1,5 @@
-//package com.plugga.backend
-//
+package com.plugga.backend
+
 //import org.junit.jupiter.api.Assertions.assertTrue
 //import org.junit.jupiter.api.Test
 //import org.springframework.boot.test.context.SpringBootTest
--- backend/src/test/kotlin/com/plugga/backend/unit/controller/UserControllerTests.kt
+++ backend/src/test/kotlin/com/plugga/backend/unit/controller/UserControllerTests.kt
@@ -4,7 +4,7 @@ import com.nhaarman.mockito_kotlin.any
 import com.plugga.backend.controller.UserController
 import com.plugga.backend.entity.User
 import com.plugga.backend.service.UserService
-import java.sql.Timestamp
+import org.hamcrest.core.Is.`is`
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -13,12 +13,18 @@ import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.http.MediaType
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import java.sql.Timestamp
+
 
 @ExtendWith(MockitoExtension::class)
 class UserControllerTests {
@@ -61,6 +67,15 @@ class UserControllerTests {
     fun `Test get User by id`() {
         given(userService.findById(any())).willReturn(validUser)
 
-        mockMvc.perform(get("/api/users/${validUser.id}")).andExpect(status().isOk)
+        mockMvc.perform(get("/api/users/${validUser.id}"))
+            .andExpect(status().isOk)
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$.id", `is`(validUser.id)))
+            .andExpect(jsonPath("$.name", `is`(validUser.name)))
+            .andExpect(jsonPath("$.email", `is`(validUser.email)))
+            .andExpect(jsonPath("$.password").doesNotExist())
+            .andExpect(jsonPath("$.dateCreated", `is`(validUser.dateCreated!!.time)))
+            .andExpect(jsonPath("$.lastLogin", `is`(validUser.lastLogin!!.time)))
     }
 }
